4ea7d879e0c96884a5c98ac138614ba9359d832d
Samuel Odigie <samodigie98@gmail.com>
2022-10-18 16:09:37 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
+const ul = document.querySelector('.cards')
+function createAndAppendPokemonCard(pokemonJSObject){
+    data.forEach((pokemonJSObject) => {
+        const li =createCardLi(pokemonJSObject.name)
+    ul.appendChild(li)
+})
 console.log(data);
+console.log(function(pokemonJSObject)
 
 //You can start simple and just render a single 
 //pokemon card from the first element
-console.log(data[0]);
+console.log(data[0]));
